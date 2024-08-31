410d719bb4e498901283f015b08ee49e3d39ea0a
hasanhafiz <hasanhafizsakil@gmail.com>
2024-08-31 18:01:42 +0600

    Update db.sql

--- database/db.sql
+++ database/db.sql
@@ -22,9 +22,6 @@ CREATE TABLE IF NOT EXISTS transactions (
 
 INSERT INTO users (user_id,`name`,email,`password`,`is_admin`) VALUES 
    ("669857aa97cbb", "Hasan Hafiz", "hasanhafizsakil@gmail.com","8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92", 1),
-   ("66be4b42571cd", "Adury Afrose", "adury@gmail.com","8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92", 0),
-   ("66be4fb505907", "Sumon Nasir", "nasir@gmail.com","8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92", 0),
-   ("66be5056058b9", "Muhammad Faijullah", "faijullah@gmail.com","8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92", 0),
    ("66c0a6d492b2d", "Faisal", "faisal@gmail.com","8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92", 0),
    ("66c0a7d486152", "Hasan Samaun", "samaun@gmail.com","8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92", 0);
    
