326554c3e68e32141afc3ba06d969e9b4f961da3
hasanhafiz <hasanhafizsakil@gmail.com>
2024-08-31 18:02:01 +0600

    Update db.sql

--- database/db.sql
+++ database/db.sql
@@ -24,13 +24,3 @@ INSERT INTO users (user_id,`name`,email,`password`,`is_admin`) VALUES
    ("669857aa97cbb", "Hasan Hafiz", "hasanhafizsakil@gmail.com","8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92", 1),
    ("66c0a6d492b2d", "Faisal", "faisal@gmail.com","8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92", 0),
    ("66c0a7d486152", "Hasan Samaun", "samaun@gmail.com","8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92", 0);
-   
-   INSERT INTO `transactions` (`id`,`user_id`,`customer_id`,`amount`,`transaction_date`,`transaction_type`) VALUES 
-("66c0a6f689a74","66c0a6d492b2d","66c0a6d492b2d","100","17 Aug 2024, 13:34:46 PM","1"),
-("66c0a6f9c9881","66c0a6f9c9881","66c0a6d492b2d","500","17 Aug 2024, 13:34:49 PM", "1"),
-("66c0a6fd28018","66c0a6d492b2d","66c0a6d492b2d","300","17 Aug 2024, 13:34:53 PM","1"),
-("66c0a7004746f","66c0a6d492b2d","66c0a6d492b2d","100","17 Aug 2024, 13:34:56 PM","1"),
-("66c0a709bd371","66c0a6d492b2d","66c0a6d492b2d","300","17 Aug 2024, 13:35:05 PM","2"),
-("66c0a70d4dc40","66c0a6d492b2d","66c0a6d492b2d","100","17 Aug 2024, 13:35:09 PM","2"),
-("66c0a72365f99","66c0a6d492b2d","66be4fb505907","300","17 Aug 2024, 13:35:31 PM","2"),
-("66c0a7236623a","66be4fb505907","66c0a6d492b2d","300","17 Aug 2024, 13:35:31 PM","1");
